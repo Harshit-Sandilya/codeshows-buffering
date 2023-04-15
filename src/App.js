3ed7033420d3c9272da811d1b2dbb3d5a6d7b6c4
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-04-15 22:52:19 +0530

    nav-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
-
+// import React from 'react';
+import home from './images/home.png';
+import event from './images/events.png';
 function App() {
   return (
-    <div>Hello</div>
+    <div className='navbar'>
+      <div className='logo'>CodeShows</div>
+      <div className='menu'>
+        <div className='home'>
+          <img src={home} alt=''></img>
+          <p>Home</p>
+          <div className='events'>
+            <img src={event} alt=''></img>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
