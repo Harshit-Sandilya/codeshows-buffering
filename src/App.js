ea91b89a781186f55de11a2e8c65566b2d19867c
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-04-17 15:12:04 +0000

    hosting routing fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import "./App.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Blog from "./Pages/Blog";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="blog" element={<Blog />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Route index element={<Home />} />
+					<Route path="login" element={<Login />} />
+					<Route path="blog" element={<Blog />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
